8e2b68a4cb25f08d02bc71846e82ad5193407555
JJHyeok13 <jjinh99@naver.com>
2024-02-23 01:29:25 +0900

    StyleSheet를 이용한 스타일링

--- App.js
+++ App.js
@@ -2,13 +2,41 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-      <Text>다른 텍스트도 집어 넣기</Text>
-      <TextInput placeholder="Type Input" />
-      <Button title="Press Button" />
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Type Input" />
+        <Button title="Press" />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 2,
+    borderBottomColor: "#CCCCCC",
+  },
+  textInput: {
+    width: "70%",
+    marginRight: 5,
+    padding: 8,
+    borderWidth: 2,
+    borderColor: "#CCCCCC",
+  },
+  listContainer: {
+    flex: 5,
+    flexDirection: "column",
+  },
+});

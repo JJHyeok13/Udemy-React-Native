bce11613743d34c6041f81116e428532d2c1927c
JJHyeok13 <jjinh99@naver.com>
2024-02-23 01:17:42 +0900

    Button, TextInput 사용하기

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hello World!</Text>
+      <Text>다른 텍스트도 집어 넣기</Text>
+      <TextInput placeholder="Type Input" />
+      <Button title="Press Button" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
